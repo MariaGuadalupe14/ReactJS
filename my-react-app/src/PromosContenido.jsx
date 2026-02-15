bccc5a24785622c965aa733faa9485db899fe72e
Maria Guadalupe Clemente Tellez <240613@utxicotepec.edu.mx>
2026-02-15 00:15:46 -0600

    ReactJS
    Organizo la sección de sucursales y ajusto la vista de mapas

--- my-react-app/src/PromosContenido.jsx
+++ my-react-app/src/PromosContenido.jsx
@@ -1,14 +1,12 @@
-import Mapa from './Mapa';
 import './PromosContenido.css';
+import MapaGeolocalizacion from './MapaGeolocalizacion';
 
 function PromosContenido() {
   return (
     <div className="promosContenido">
       <h2>Promociones</h2>
       <p>Contenido de promociones aquÃ­</p>
-      <Mapa lat={20.276247346277895} 
-      lng={-97.95828426659142} 
-      nombre_sucursal={'Sucursal Centro'} />
+      <MapaGeolocalizacion />
     </div>
   );
 }
--- my-react-app/src/Sucursales.jsx
+++ my-react-app/src/Sucursales.jsx
@@ -1,37 +1,64 @@
-﻿import Mapa from './Mapa';
+import Mapa from './Mapa';
 import './Sucursales.css';
+
+const SUCURSALES = [
+  {
+    nombre: 'Sucursal Centro',
+    direccion: 'Calle Hidalgo 123, Col. Centro, Poza Rica, Veracruz',
+    telefono: '+52 782 000 0001',
+    imagen: '',
+    latitud: 20.27657415896826,
+    longitud: -97.9642692798461,
+  },
+  {
+    nombre: 'Sucursal Norte',
+    direccion: 'Av. 20 de Noviembre 456, Col. Obrera, Poza Rica, Veracruz',
+    telefono: '+52 782 000 0002',
+    imagen: '',
+    latitud: 20.276191177580813,
+    longitud: -97.95967610900618,
+  },
+  {
+    nombre: 'Sucursal Sur',
+    direccion: 'Calle 16 de Septiembre 789, Col. Petromex, Poza Rica, Veracruz',
+    telefono: '+52 782 000 0003',
+    imagen: '',
+    latitud: 20.275604735498444,
+    longitud: -97.96429479746189,
+  },
+  {
+    nombre: 'Sucursal Este',
+    direccion: 'Boulevard Adolfo Ruiz Cortines 321, Col. Tajin, Poza Rica, Veracruz',
+    telefono: '+52 782 000 0004',
+    imagen: '',
+    latitud: 20.275640456995326,
+    longitud: -97.95872224112566,
+  },
+];
 
 function Sucursales() {
   return (
     <section className="sucursales">
       <h2>Nuestras Sucursales</h2>
       <div className="lista-sucursales">
-        <Mapa
-          lat={20.27657415896826}
-          lng={-97.9642692798461}
-          nombre_sucursal={'Sucursal Centro'}
-          direccion={'Calle Hidalgo 123, Col. Centro, Poza Rica, Veracruz'}
-        />
-        <Mapa
-          lat={20.276191177580813}
-          lng={-97.95967610900618}
-          nombre_sucursal={'Sucursal Norte'}
-          direccion={'Av. 20 de Noviembre 456, Col. Obrera, Poza Rica, Veracruz'}
-        />
-        <Mapa
-          lat={20.275604735498444}
-          lng={-97.96429479746189}
-          nombre_sucursal={'Sucursal Sur'}
-          direccion={'Calle 16 de Septiembre 789, Col. Petromex, Poza Rica, Veracruz'}
-        />
-        <Mapa
-          lat={20.275640456995326}
-          lng={-97.95872224112566}
-          nombre_sucursal={'Sucursal Este'}
-          direccion={'Boulevard Adolfo Ruiz Cortines 321, Col. Tajin, Poza Rica, Veracruz'}
-        />
+        {SUCURSALES.map((sucursal) => (
+          <TarjetaSucursal key={sucursal.nombre} {...sucursal} />
+        ))}
       </div>
     </section>
+  );
+}
+
+function TarjetaSucursal({ nombre, direccion, telefono, imagen, latitud, longitud }) {
+  return (
+    <article className="sucursal">
+      {imagen ? <img src={imagen} alt={`Logo de ${nombre}`} /> : null}
+      <h3>{nombre}</h3>
+      <p className="direccion">{direccion}</p>
+      <Mapa lat={latitud} lng={longitud} nombre_sucursal={nombre} />
+      <p>{telefono}</p>
+      <a href="#">Ver mas</a>
+    </article>
   );
 }
 
